6e112efc6c90a57a6e09bb8b7a564d8e90e99f38
Joe Anderson <joe@metaphorcreations.com>
2025-02-24 16:34:43 -0600

    Updates

--- includes/mtphr-settings/assets/build/mtphrSettings.asset.php
+++ includes/mtphr-settings/assets/build/mtphrSettings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f46ba84a7535c872178e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e9da3f02dfc28b8c0d1f');
